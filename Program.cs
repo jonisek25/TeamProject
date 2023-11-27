eeee5d2c87923962e242b4e2682e082def0b59ed
AlesProgramming <alesrhensel@gmail.com>
2023-11-27 11:02:53 +0100

    Ales3

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿for (int x = 0; x < 29; x++)
+{
+    Console.WriteLine("I broke it but I also fixed it so we good");
+}
